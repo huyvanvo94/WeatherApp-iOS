bad2ef1cfe04d183718fb0689f987d31fe91864a
Huy Vo <huyvanvo94@gmail.com>
2017-12-10 18:41:00 -0800

    Update LocationService.swift

--- WeatherApp/LocationService.swift
+++ WeatherApp/LocationService.swift
@@ -38,7 +38,7 @@ class LocationService: NSObject, CLLocationManagerDelegate {
         }
         
         locationManager.desiredAccuracy = kCLLocationAccuracyBest // The accuracy of the location data
-        locationManager.distanceFilter = 200 // The minimum distance (measured in meters) a device must move horizontally before an update event is generated.
+        locationManager.distanceFilter = 500 // The minimum distance (measured in meters) a device must move horizontally before an update event is generated.
         locationManager.delegate = self
     }
     
